9317265e6389848018e3ed1209872a800fc711a9
Roland Vachter <roland.vachter@ft.com>
2017-01-10 18:03:41 +0200

    Fix rounding float numbers when closer than 0.1 from the next number.

--- assets/js/app/filters.js
+++ assets/js/app/filters.js
@@ -15,6 +15,16 @@ module.filter('float', ['$sce', function($sce) {
 		}
 
 		const decimal = Math.abs(value) % 1;
+
+		if (decimal && decimal.toFixed(1) === '1.0') {
+			decimal = 0;
+
+			if (value >= 0) {
+				transformedValue += Math.floor(value) + 1;
+			} else {
+				transformedValue += Math.ceil(value) - 1;
+			}
+		}
 		transformedValue += `<span class="decimal">${decimal ? decimal.toFixed(1).substr(1) : '.0'}</span>`;
 
 		return $sce.trustAsHtml(transformedValue);
