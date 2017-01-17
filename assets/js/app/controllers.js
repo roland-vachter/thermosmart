7698ce8e7a493d69403a0f2aa1fcf1368762ddda
Roland Vachter <roland.vachter@ft.com>
2017-01-17 20:23:18 +0200

    Color the chart.

--- assets/js/app/controllers.js
+++ assets/js/app/controllers.js
@@ -188,7 +188,22 @@ module.controller('mainCtrl', ['$scope', '$http', 'socketio', 'loginStatus', fun
 								y: $scope.isHeatingOn
 							}
 						],
-						steppedLine: true
+						steppedLine: true,
+						backgroundColor: "rgba(75,192,192,0.4)",
+						borderColor: "rgba(75,192,192,1)",
+						borderCapStyle: 'butt',
+						borderDash: [],
+						borderDashOffset: 0.0,
+						borderJoinStyle: 'miter',
+						pointBorderColor: "rgba(75,192,192,1)",
+						pointBackgroundColor: "#fff",
+						pointBorderWidth: 1,
+						pointHoverRadius: 6,
+						pointHoverBackgroundColor: "rgba(75,192,192,1)",
+						pointHoverBorderColor: "rgba(220,220,220,1)",
+						pointHoverBorderWidth: 2,
+						pointRadius: 3,
+						pointHitRadius: 9,
 					}]
 				},
 				options: {
