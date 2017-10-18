df3f11f23cb16de7e7ac33f3d658133155b02ba4
Roland Vachter <roland.vachter@iquestgroup.com>
2017-10-18 18:35:58 +0300

    Show statistics for the last month in the statistics overlay.

--- assets/js/app/controllers.js
+++ assets/js/app/controllers.js
@@ -1,4 +1,4 @@
-/* global $, Chart */
+/* global $, Chart, moment */
 
 'use strict';
 
@@ -208,6 +208,146 @@ module.controller('mainCtrl', ['$scope', '$http', 'socketio', 'loginStatus', 'pa
 			$scope.statisticsForToday = data.statisticsForToday;
 		}
 
+		if ((data.statisticsForLastMonth && data.statisticsForLastMonth.length && !isMobileApp) || initialData) {
+			const minTargetTemp = Math.min(...data.statisticsForLastMonth.map(item => item.avgTargetTemp)).toFixed(1);
+			const maxTargetTemp = Math.max(...data.statisticsForLastMonth.map(item => item.avgTargetTemp)).toFixed(1);
+
+			const minAvgOutsideTemp = Math.min(...data.statisticsForLastMonth.map(item => item.avgOutsideTemp)).toFixed(1);
+			const maxAvgOutsideTemp = Math.max(...data.statisticsForLastMonth.map(item => item.avgOutsideTemp)).toFixed(1);
+
+			new Chart(document.querySelector('#statisticsLast30Days'), {
+				type: 'line',
+				options: {
+					maintainAspectRatio: false,
+					responsive: true
+				},
+				data: {
+					datasets: [{
+						label: 'Heating running time',
+						yAxisID: "duration",
+						data: [
+							...data.statisticsForLastMonth.map(item => {return {x: item.date, y: item.runningMinutes}; })
+						],
+						backgroundColor: "rgba(75,192,192,0.4)",
+						borderColor: "rgba(75,192,192,1)",
+						borderCapStyle: 'butt',
+						borderDash: [],
+						borderDashOffset: 0.0,
+						borderJoinStyle: 'miter',
+						pointBorderColor: "rgba(75,192,192,1)",
+						pointBackgroundColor: "#fff",
+						pointBorderWidth: 1,
+						pointHoverRadius: 6,
+						pointHoverBackgroundColor: "rgba(75,192,192,1)",
+						pointHoverBorderColor: "rgba(220,220,220,1)",
+						pointHoverBorderWidth: 2,
+						pointRadius: 3,
+						pointHitRadius: 9,
+					}, {
+						label: 'Avg target temperature',
+						yAxisID: "temp",
+						data: [
+							...data.statisticsForLastMonth.map(item => {return {x: item.date, y: item.avgTargetTemp}; })
+						],
+						borderColor: "rgba(255,116,0,1)",
+						borderCapStyle: 'butt',
+						borderDash: [],
+						borderDashOffset: 0.0,
+						borderJoinStyle: 'miter',
+						pointBorderColor: "rgba(255,116,0,1)",
+						pointBackgroundColor: "#fff",
+						pointBorderWidth: 1,
+						pointHoverRadius: 6,
+						pointHoverBackgroundColor: "rgba(255,116,0,1)",
+						pointHoverBorderColor: "rgba(220,220,220,1)",
+						pointHoverBorderWidth: 2,
+						pointRadius: 3,
+						pointHitRadius: 9,
+						fill: false,
+					}, {
+						label: 'Avg outside temperature',
+						yAxisID: "temp",
+						data: [
+							...data.statisticsForLastMonth.map(item => {return {x: item.date, y: item.avgOutsideTemp}; })
+						],
+						borderColor: "rgba(115,136,10,1)",
+						borderCapStyle: 'butt',
+						borderDash: [],
+						borderDashOffset: 0.0,
+						borderJoinStyle: 'miter',
+						pointBorderColor: "rgba(115,136,10,1)",
+						pointBackgroundColor: "#fff",
+						pointBorderWidth: 1,
+						pointHoverRadius: 6,
+						pointHoverBackgroundColor: "rgba(115,136,10,1)",
+						pointHoverBorderColor: "rgba(220,220,220,1)",
+						pointHoverBorderWidth: 2,
+						pointRadius: 3,
+						pointHitRadius: 9,
+						fill: false,
+					}]
+				},
+				options: {
+					scales: {
+						xAxes: [{
+							type: 'time',
+							time: {
+								unit: 'day',
+								tooltipFormat: 'MMM Do',
+								unitStepSize: 1,
+								displayFormats: {
+									millisecond: 'SSS [ms]',
+									second: 'h:mm:ss a',
+									minute: 'h:mm:ss a',
+									hour: 'HH:mm',
+									day: 'MMM D',
+									week: 'll',
+									month: 'MMM YYYY',
+									quarter: '[Q]Q - YYYY',
+									year: 'YYYY'
+								}
+							}
+						}],
+						yAxes: [{
+							id: "duration",
+							ticks: {
+								callback: value => {
+									let str = '';
+
+									const duration = moment.duration(value * 60 * 1000);
+
+									str += `${duration.hours() || 0}h `;
+									str += `${duration.minutes() || 0}m`;
+
+									return str.trim();
+								},
+								fixedStepSize: 15,
+								min: 0
+							},
+							tooltipFormat: value => {
+								let str = '';
+
+								const duration = moment.duration(value * 60 * 1000);
+
+								str += `${duration.hours() || 0}h `;
+								str += `${duration.minutes() || 0}m`;
+
+								return str.trim();
+							}
+						}, {
+							id: "temp",
+							ticks: {
+								callback: value => value,
+								fixedStepSize: 1,
+								min: Math.floor(Math.min(minTargetTemp, minAvgOutsideTemp)),
+								max: Math.ceil(Math.max(maxTargetTemp, maxAvgOutsideTemp))
+							}
+						}]
+					}
+				}
+			});
+		}
+
 		if ((data.heatingHistoryLast24 && data.heatingHistoryLast24.length && !isMobileApp) || initialData) {
 			new Chart(document.querySelector('#heatingHistoryChart'), {
 				type: 'line',
@@ -260,7 +400,7 @@ module.controller('mainCtrl', ['$scope', '$http', 'socketio', 'loginStatus', 'pa
 									second: 'h:mm:ss a',
 									minute: 'h:mm:ss a',
 									hour: 'HH:mm',
-									day: 'll',
+									day: 'MMM D',
 									week: 'll',
 									month: 'MMM YYYY',
 									quarter: '[Q]Q - YYYY',
